3b8bdbda4a8e3a15664340d1d289d22f515b8352
Alex <alex.ots@gmail.com>
2022-11-06 22:00:03 +0200

    Update tt_api.py

--- tt_api.py
+++ tt_api.py
@@ -6,6 +6,7 @@ class TikTokWrapper:
         self.loop = asyncio.new_event_loop()
            
     def get_video(self, video_id):
-        with TikTokApi(asyncio=asyncio.set_event_loop(self.loop)) as api:
+        verify_fp = 'verify_la5rx1mw_YIjueqLL_0Qhz_4m1N_ATBb_5OQVgSLbFYAq'
+        with TikTokApi(asyncio=asyncio.set_event_loop(self.loop), custom_verify_fp=verify_fp, use_test_endpoints=True) as api:
             video_data = api.video(id=video_id)
             return video_data.bytes()
