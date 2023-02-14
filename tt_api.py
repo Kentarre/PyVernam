15038f8a2cba2e445ab600b3fd01681f5e110bbf
Alex <alex.ots@gmail.com>
2023-02-14 20:19:23 +0200

    Update tt_api.py

--- tt_api.py
+++ tt_api.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 from TikTokApi import TikTokApi
 
 class TikTokWrapper:
@@ -7,6 +8,7 @@ class TikTokWrapper:
            
     def get_video(self, video_id):
         verify_fp = 'verify_le4j0oq7_rfWhr2n3_m8tJ_4bMB_80w0_VH7pMVZYnxyO'
-        with TikTokApi(asyncio=asyncio.set_event_loop(self.loop), custom_verify_fp=verify_fp, use_test_endpoints=True) as api:
+        did = str(random.randint(10000, 999999999))
+        with TikTokApi(asyncio=asyncio.set_event_loop(self.loop), custom_verify_fp=verify_fp, custom_device_id=did) as api:
             video_data = api.video(id=video_id)
             return video_data.bytes()
